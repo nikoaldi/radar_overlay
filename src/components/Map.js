96f0ebbec241a5aa8848d31d015889602e17e9c9
Saeful Abdulloh Sayuti <saepulabdullohs@gmail.com>
2023-08-03 15:58:16 +0700

    Feat (Radar) : add new function in radar video

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,85 +1,109 @@
-import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import './leaflet-geojson-vt';
+import _ from 'lodash';
 
 function MapTest() {
     const mapRef = useRef(null);
     const websocketRef = useRef(null);
     const vectorTileLayerRef = useRef(null);
-  
+    const [opacity, setOpacity] = useState(1.0);
 
     useEffect(() => {
+        const THROTTLE_TIME = 40; // Throttle time in milliseconds
 
         // Set up the Leaflet map if it doesn't exist
         if (!mapRef.current) {
-            const map = L.map('map').setView([-6.949639442878905 , 107.61965170455426], 12);
+            const map = L.map('map').setView([-9.2893, 106.4583], 11);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
             mapRef.current = map;
         }
 
         // Connect to the WebSocket server
-        websocketRef.current = new WebSocket('ws://localhost:8080/geosocket');
+        websocketRef.current = new WebSocket('ws://172.16.25.219:8080/geosocket');
+
+        let throttleHandle;
+        let dataQueue = [];
 
         // Handle WebSocket connection open event
         websocketRef.current.onopen = () => {
             console.log('WebSocket connected');
         };
-    
+
         // Handle WebSocket message event
         websocketRef.current.onmessage = (event) => {
-            // Received GeoJSON data from the WebSocket
-            let data;
-            try {
-                data = JSON.parse(event.data);
-                console.log(data)
-            } catch (error) {
-                console.error('Invalid JSON data received:', error);
-                return;
-            }
+            const data = JSON.parse(event.data);
 
-            const options = {
-                maxZoom: 19,
-                tolerance: 50,
-                debug: 0,
-                style: {
-                weight: 2,
-                fillColor: '#00ff33',
-                color: '#F2FF00',
-                },
-            };
-  
-            // Remove the existing vector tile layer if it exists
-            if (vectorTileLayerRef.current) {
-                mapRef.current.removeLayer(vectorTileLayerRef.current);
+            if (data.features && data.features.length > 0) {
+                // Add received data to the queue
+                dataQueue.push(data);
+
+                // Throttle the data handling
+                if (!throttleHandle) {
+                    throttleHandle = setTimeout(() => {
+                        processGeoJSONData(dataQueue);
+                        throttleHandle = null;
+                        dataQueue = [];
+                    }, THROTTLE_TIME);
                 }
-
-            // Add the vector tiles layer to the map
-            const vectorTileLayer = L.geoJson.vt(data, options).addTo(mapRef.current);
-
-            // Add the vector tile layer to the map
-            vectorTileLayer.addTo(mapRef.current);
-
-            // Save the vector tile layer reference
-            vectorTileLayerRef.current = vectorTileLayer;
+            }
         };
 
+        // In the useEffect hook, update the part where you create the vectorTileLayer:
+        const processGeoJSONData = (dataQueue) => {
+            const data = _.merge({}, ...dataQueue); // Merge all GeoJSON data into one
+            const options = (feature) => {
+                const properties = feature.properties || {}; // Ensure properties object exists
+            
+                // Provide default values if properties are missing
+                const strokeColor = properties.stroke || '#000000';
+                const fillColor = properties.fill || '#000000';
+                const weight = properties.weight || 4;
+                const opacity = properties.opacity || 1.0;
+                const fillOpacity = properties.fillOpacity || 1.0;
+            
+                return {
+                    weight,
+                    fillColor,
+                    color: strokeColor,
+                    fillOpacity,
+                    opacity,
+                };
+            };            
+
+            if (vectorTileLayerRef.current) {
+                // If the vector tile layer already exists, update it with new data
+                // vectorTileLayerRef.current.clearLayers();
+                vectorTileLayerRef.current.addLayer(L.geoJson(data, { style: options })); // Add the GeoJSON layer to the Layer Group
+            } else {
+                // Create a new Layer Group and add the GeoJSON layer to it
+                const vectorTileLayerGroup = L.layerGroup();
+                vectorTileLayerGroup.addLayer(L.geoJson(data, { style: options }));
+                vectorTileLayerGroup.addTo(mapRef.current);
+                vectorTileLayerRef.current = vectorTileLayerGroup;
+            }
+
+            // Limit the number of features displayed
+            if (vectorTileLayerRef.current.getLayers().length > 2048) {
+                mapRef.current.removeLayer(vectorTileLayerRef.current);
+                vectorTileLayerRef.current = null;
+            }
+        };
         // Handle WebSocket error event
         websocketRef.current.onerror = (error) => {
             console.error('WebSocket error:', error);
         };
-  
+
         // Clean up the WebSocket connection on unmount
         return () => {
+            clearTimeout(throttleHandle);
             websocketRef.current.close();
         };
-    
-
     }, []);
 
-
-    return <div id="map" style={{width: '100', height: '100vh'}}></div>;
+    return <div id="map" style={{ width: '100', height: '100vh' }}></div>;
 }
 
 export default MapTest;
